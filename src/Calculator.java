b7e02a53413d9b247808f332b326725e1b788b7d
jgbuitragoc <j.buitrago@globant.com>
2024-08-09 13:34:24 -0500

    multiplication and division added

--- src/Calculator.java
+++ src/Calculator.java
@@ -1,2 +1,12 @@
 public class Calculator {
+    public float multiplication(float a, float b) {
+        return a * b;
+    }
+
+    public float division(float a, float b) {
+        if (b != 0) {
+            return a / b;
+        }
+        return 0f;
+    }
 }
